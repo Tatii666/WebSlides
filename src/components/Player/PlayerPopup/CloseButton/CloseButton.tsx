6d749c28e16d19f1d8ffeeb95ada9aeeb0400a79
Tatii666 <tatianabezgina@icloud.com>
2021-10-13 23:26:33 +0300

    fp-5. Сделала вьюшку компоненты Плеер. Добавлен навигационный попап превью.

---
+++ src/components/Player/PlayerPopup/CloseButton/CloseButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './CloseButton.module.css';
+
+function CloseButton (){
+    return (
+        <div className={s.closeButton}>
+
+        </div>
+    );
+}
+
+export {CloseButton};
---
+++ src/components/Player/SlideContent/SlideContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './SlideContent.module.css';
+
+function SlideContent() {
+    return (
+        <div className={s.slideContent}>
+            Наш Плеер
+        </div>
+    );
+}
+
+
+export {SlideContent};
