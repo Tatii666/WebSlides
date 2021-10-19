7a6ec6aae5dc77d05e774ee476b0629e922dcaa4
Tatii666 <tatianabezgina@icloud.com>
2021-10-19 20:30:32 +0300

    fp-3. добавила в наш editorArea компоненту, которая будет отображать содержимое слайда

--- src/components/Editor/EditorArea/EditorArea.tsx
+++ src/components/Editor/EditorArea/EditorArea.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './EditorArea.module.css';
+import {SlideCanvas} from "./SlideCanvas/SlideCanvas";
 
 function EditorArea() {
     return (
         <div className={s.editorArea}>
-            Наше рабочее поле
+            <SlideCanvas />
         </div>
     );
 }
